67dc30e893bf534cf312f1ed3ece39eaed0ac6e5
Douglas Uretsky <douglasuretsky@gmail.com>
2018-11-13 15:46:36 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
       item = @@items.find{|item| item.name == item_name}
-      
+      if item
     end
   end
 end
