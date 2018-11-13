48520cbcb06814c6bd37854b5bafa39466ba3f08
Douglas Uretsky <douglasuretsky@gmail.com>
2018-11-13 15:41:51 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,7 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
     if req.path.match(/items/)
-      item_name = 
+      item_name = req.path.split("/items/").last
     end
   end
 end
